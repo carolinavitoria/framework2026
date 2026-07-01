e08bbd1a71dda21dad9185fb8bef55cd26fd1fd5
unknown <Ifro@GJM-LAB6-33.ifro.local>
2026-07-01 08:11:38 -0400

    teste

---
+++ app/Controllers/Paginas.php
@@ -0,0 +1,20 @@
+<?php
+
+class Paginas extends Controller{
+    public function index(){
+        $dados = ['titulo'=>"Página Inicial",
+                  'descricao' => "Aula de PW com  3º matutino, php orientado a objetos"
+        ];
+        $this->view('paginas/home', $dados);
+    }//fim da função index
+
+    public function sobre(){
+        $dados = ['titulo'=>"Página Sobre nós",
+                  'descricao'=>"Aqui você encontra as noticias mais confiaveis..."
+        ];
+        $this->view('paginas/sobre', $dados);
+    }//fim da função sobre
+
+}//fim da classe Paginas
+
+?>
---
+++ app/Views/paginas/sobre.php
@@ -0,0 +1,4 @@
+<h1><?php echo $dados['titulo'];?></h1>
+<p><?php echo $dados['descricao'];?><p>
+    
+    <?= APP_VERSAO ?>
